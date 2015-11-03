0a7b24a8d57aaff55915a4a4936c4b87001a2631
Thomas Richner <thomasrichner+github@gmail.com>
2015-11-04 00:36:23 +0100

    cleanup

--- src/main/java/ch/k42/rand/Range.java
+++ src/main/java/ch/k42/rand/Range.java
@@ -3,8 +3,7 @@ package ch.k42.rand;
 /**
  * @author trichner
  */
-public class Range<T> implements Comparable<Object> {
-
+public class Range<T>{
     final int bottom,top;
     final WeightedItem<T> item;
 
@@ -12,26 +11,5 @@ public class Range<T> implements Comparable<Object> {
         this.bottom = bottom;
         this.top = top;
         this.item = item;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        if(o instanceof Range<?>) {
-            Range<?> other = (Range<?>) o;
-            if(this.bottom > other.top)
-                return 1;
-            if(this.top < other.bottom)
-                return -1;
-            return 0; // overlapping ranges are considered equal.
-        } else if (o instanceof Integer) {
-            Integer other = (Integer) o;
-            if(this.bottom > other.intValue())
-                return 1;
-            if(this.top < other.intValue())
-                return -1;
-            return 0;
-        }
-        throw new IllegalArgumentException(String.format("Cannot compare Range objects to %s objects.",
-                o.getClass().getName()));
     }
 }
